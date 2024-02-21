67960d1be96458c76f0db227f36491b7f62452c2
GerardoGRCessum <p-gerardo.gonzalez@cessum.mx>
2024-02-20 17:52:42 -0700

    añadido entidades maestro, materia y remodelación de entidad estudiante. mejoramiento de seguridad JWT

--- src/main/java/com/example/demo/student/Controller/StudentController.java
+++ src/main/java/com/example/demo/student/Controller/StudentController.java
@@ -4,13 +4,16 @@ import com.example.demo.student.Entity.Student;
 import com.example.demo.student.Service.StudentService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Optional;
 
 
 @RestController
@@ -32,24 +35,50 @@ public class StudentController {
     @PostMapping
     public ResponseEntity<?> create(@Valid @RequestBody Student student, BindingResult result) {
         if (result.hasFieldErrors()){
-            return validation
+            return validation(result);
         }
+        student.setAdmin(false);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(studentService.save(student));
     }
 
-    @DeleteMapping(path = "{studentId}")
-    public void deleteStudent(@PathVariable("studentId") Long studentId) {
-        studentService.deleteStudent(studentId);
+
+
+    @PreAuthorize("hasRoles('ADMIN')")
+    @DeleteMapping(path = "/{studentId}")
+    public ResponseEntity<?> deleteStudent(@PathVariable("studentId") Long studentId) {
+        Optional<Student> studentOptional = studentService.delete(studentId);
+        if (studentOptional.isPresent()) {
+            return ResponseEntity.ok(studentOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
     }
 
-    @PutMapping(path = "{studentId}")
-    public void updateStudent(
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping(path = "/{studentId}")
+    public ResponseEntity<?> update(@Valid @RequestBody Student student, BindingResult result,
+                                    @PathVariable Long studentId){
+
+        Optional<Student> studentOptional = studentService.update(studentId, student);
+        if(result.hasFieldErrors()){
+            return ResponseEntity.status(HttpStatus.CREATED).body(studentOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    /*public void updateStudent(
             @PathVariable("studentId") Long studentId,
             @RequestParam(required = false) String name,
             @RequestParam(required = false) String email) {
-        studentService.updateStudent(studentId, name, email);
-    }
+        studentService.update(studentId, )
+    } */
 
     private ResponseEntity<?> validation(BindingResult result){
-        Map<String >
+        Map<String, String> errors = new HashMap<>();
+
+        result.getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), "El campo" + err.getField() + " " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
---
+++ src/main/java/com/example/demo/student/Service/JpaStudentDetailsService.java
@@ -0,0 +1,51 @@
+package com.example.demo.student.Service;
+
+import com.example.demo.student.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.example.demo.student.Repository.studentRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class JpaStudentDetailsService implements UserDetailsService {
+
+    @Autowired
+    private studentRepository repository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Student> studentOptional = repository.findByUsername(username);
+
+        if(studentOptional.isEmpty()){
+            throw new UsernameNotFoundException(String.format("username %s no existe en el sistema",
+                    username));
+        }
+
+        Student student = studentOptional.orElseThrow();
+
+        List<GrantedAuthority> authorities = student.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        return new User(
+                student.getUsername(),
+                student.getPassword(),
+                student.isEnable(),
+                true,
+                true,
+                true,
+                authorities);
+    }
+}
