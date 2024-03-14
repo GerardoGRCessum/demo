92c472e6e2e60527ee8baae122326205b91385de
GerardoGRCessum <p-gerardo.gonzalez@cessum.mx>
2024-03-14 10:52:17 -0700

    editada 0.1 de class Materia y Maestro

--- src/main/java/com/example/demo/student/Entity/Maestro.java
+++ src/main/java/com/example/demo/student/Entity/Maestro.java
@@ -1,4 +1,56 @@
 package com.example.demo.student.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import java.util.List;
+
+@Entity
+@EnableAutoConfiguration
+@Table(name = "teachers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Maestro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    @NotBlank
+    @Size(min = 4, max = 60)
+    private String username;
+
+    private String email;
+
+    @NotBlank
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) //no mostrar dato en json get
+    private String password;
+
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+    //TODO: añadir relacion para conectar maestro a materias y grupos
+    private String clave_materia;
+
+    private List<Role> roles;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean enable;
+
+    @PrePersist
+    public void prePersist(){
+        enable = true;
+    }
+
+    @Transient
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean admin;
+
+
 }
--- src/main/java/com/example/demo/student/Entity/Materia.java
+++ src/main/java/com/example/demo/student/Entity/Materia.java
@@ -1,4 +1,23 @@
 package com.example.demo.student.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@Entity
+@EnableAutoConfiguration
+@Table(name = "materias")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Materia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
