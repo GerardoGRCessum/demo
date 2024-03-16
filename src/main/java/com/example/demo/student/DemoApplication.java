9d3f025a8e82aee17a54611811f258b518a57491
GerardoGRCessum <p-gerardo.gonzalez@cessum.mx>
2024-03-16 13:19:09 -0700

    modificados app.properties

--- src/main/java/com/example/demo/student/DemoApplication.java
+++ src/main/java/com/example/demo/student/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan("com.example")
